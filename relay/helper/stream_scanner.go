f229804b95e5450a691794ffbe41d6bd81253c87
liuzhiguo630 <410915218@qq.com>
2025-04-10 14:45:23 +0800

    Fix: 过长的返回无法响应的问题

--- relay/helper/stream_scanner.go
+++ relay/helper/stream_scanner.go
@@ -41,7 +41,7 @@ func StreamScannerHandler(c *gin.Context, resp *http.Response, info *relaycommon
 
 	scanner.Split(bufio.ScanLines)
 	SetEventStreamHeaders(c)
-
+	scanner.Buffer(make([]byte, 64*1024), 10*1024*1024)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
