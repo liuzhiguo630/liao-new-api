7a0081fc19bb0dff8a7e3d26453e2f37dff6de5a
liuzhiguo630 <410915218@qq.com>
2025-05-24 18:00:29 +0800

    fix: 修复 concurrent map read and map write 问题

--- common/group-ratio.go
+++ common/group-ratio.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"encoding/json"
+	"sync"
 )
 
 var GroupRatio = map[string]float64{
@@ -9,8 +10,12 @@ var GroupRatio = map[string]float64{
 	"vip":     1,
 	"svip":    1,
 }
+var groupRatioMutex sync.RWMutex
 
 func GroupRatio2JSONString() string {
+	groupRatioMutex.RLock()
+	defer groupRatioMutex.RUnlock()
+
 	jsonBytes, err := json.Marshal(GroupRatio)
 	if err != nil {
 		SysError("error marshalling model ratio: " + err.Error())
@@ -19,11 +24,16 @@ func GroupRatio2JSONString() string {
 }
 
 func UpdateGroupRatioByJSONString(jsonStr string) error {
+	groupRatioMutex.Lock()
+	defer groupRatioMutex.Unlock()
+
 	GroupRatio = make(map[string]float64)
 	return json.Unmarshal([]byte(jsonStr), &GroupRatio)
 }
 
 func GetGroupRatio(name string) float64 {
+	groupRatioMutex.RLock()
+	defer groupRatioMutex.RUnlock()
 	ratio, ok := GroupRatio[name]
 	if !ok {
 		SysError("group ratio not found: " + name)
