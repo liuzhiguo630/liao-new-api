cc1928a1489cc6915e07a49ef0924283fc96ffaf
liuzhiguo630 <410915218@qq.com>
2025-08-22 00:02:03 +0800

    feat: 修复并发 map 问题

--- setting/user_usable_group.go
+++ setting/user_usable_group.go
@@ -3,14 +3,21 @@ package setting
 import (
 	"encoding/json"
 	"one-api/common"
+	"sync"
 )
 
-var userUsableGroups = map[string]string{
-	"default": "默认分组",
-	"vip":     "vip分组",
-}
+var (
+	userUsableGroups = map[string]string{
+		"default": "默认分组",
+		"vip":     "vip分组",
+	}
+	userUsableGroupsMutex sync.RWMutex
+)
 
 func GetUserUsableGroupsCopy() map[string]string {
+	userUsableGroupsMutex.RLock()
+	defer userUsableGroupsMutex.RUnlock()
+
 	copyUserUsableGroups := make(map[string]string)
 	for k, v := range userUsableGroups {
 		copyUserUsableGroups[k] = v
@@ -19,6 +26,9 @@ func GetUserUsableGroupsCopy() map[string]string {
 }
 
 func UserUsableGroups2JSONString() string {
+	userUsableGroupsMutex.RLock()
+	defer userUsableGroupsMutex.RUnlock()
+
 	jsonBytes, err := json.Marshal(userUsableGroups)
 	if err != nil {
 		common.SysError("error marshalling user groups: " + err.Error())
@@ -27,8 +37,17 @@ func UserUsableGroups2JSONString() string {
 }
 
 func UpdateUserUsableGroupsByJSONString(jsonStr string) error {
-	userUsableGroups = make(map[string]string)
-	return json.Unmarshal([]byte(jsonStr), &userUsableGroups)
+	newGroups := make(map[string]string)
+	err := json.Unmarshal([]byte(jsonStr), &newGroups)
+	if err != nil {
+		return err
+	}
+
+	userUsableGroupsMutex.Lock()
+	userUsableGroups = newGroups
+	userUsableGroupsMutex.Unlock()
+
+	return nil
 }
 
 func GetUserUsableGroups(userGroup string) map[string]string {
@@ -47,11 +66,17 @@ func GetUserUsableGroups(userGroup string) map[string]string {
 }
 
 func GroupInUserUsableGroups(groupName string) bool {
+	userUsableGroupsMutex.RLock()
+	defer userUsableGroupsMutex.RUnlock()
+
 	_, ok := userUsableGroups[groupName]
 	return ok
 }
 
 func GetUsableGroupDescription(groupName string) string {
+	userUsableGroupsMutex.RLock()
+	defer userUsableGroupsMutex.RUnlock()
+
 	if desc, ok := userUsableGroups[groupName]; ok {
 		return desc
 	}
