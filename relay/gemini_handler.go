f1db1da794be18b4aacb92adbd159a405305e4ae
liuzhiguo630 <410915218@qq.com>
2025-11-20 22:09:01 +0800

    feat: 修复 gemini minthink 和一些参数冲突的问题

--- relay/gemini_handler.go
+++ relay/gemini_handler.go
@@ -149,9 +149,7 @@ func GeminiHelper(c *gin.Context, info *relaycommon.RelayInfo) (newAPIError *typ
 			if info.ParamOverride["minThink"] != nil {
 				budget := info.ParamOverride["minThink"].(map[string]interface{})[info.OriginModelName]
 				if budget != nil {
-					if request.GenerationConfig.ThinkingConfig != nil {
-						request.GenerationConfig.ThinkingConfig.ThinkingBudget = common.GetPointer(int(budget.(float64)))
-					} else {
+					if request.GenerationConfig.ThinkingConfig == nil {
 						request.GenerationConfig.ThinkingConfig = &dto.GeminiThinkingConfig{
 							ThinkingBudget: common.GetPointer(int(budget.(float64))),
 						}
