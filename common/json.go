18a8fcb01c053c5fc20cc16f9fce9972216186d4
liuzhiguo630 <410915218@qq.com>
2026-02-28 13:58:34 +0800

    feat: 优化 json 性能

--- common/json.go
+++ common/json.go
@@ -4,22 +4,24 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+
+	"github.com/bytedance/sonic"
 )
 
 func Unmarshal(data []byte, v any) error {
-	return json.Unmarshal(data, v)
+	return sonic.Unmarshal(data, v)
 }
 
 func UnmarshalJsonStr(data string, v any) error {
-	return json.Unmarshal(StringToByteSlice(data), v)
+	return sonic.UnmarshalString(data, v)
 }
 
 func DecodeJson(reader io.Reader, v any) error {
-	return json.NewDecoder(reader).Decode(v)
+	return sonic.ConfigDefault.NewDecoder(reader).Decode(v)
 }
 
 func Marshal(v any) ([]byte, error) {
-	return json.Marshal(v)
+	return sonic.Marshal(v)
 }
 
 func GetJsonType(data json.RawMessage) string {
