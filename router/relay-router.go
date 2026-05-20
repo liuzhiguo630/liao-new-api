b0b8c04f242aa1dda3ec1106b9acf6e2711f3520
liuzhiguo630 <410915218@qq.com>
2026-05-20 14:48:29 +0800

    feat: support realtime translation models

--- router/relay-router.go
+++ router/relay-router.go
@@ -78,6 +78,9 @@ func SetRelayRouter(router *gin.Engine) {
 		wsRouter.GET("/realtime", func(c *gin.Context) {
 			controller.Relay(c, types.RelayFormatOpenAIRealtime)
 		})
+		wsRouter.GET("/realtime/translations", func(c *gin.Context) {
+			controller.Relay(c, types.RelayFormatOpenAIRealtime)
+		})
 	}
 	{
 		//http router
